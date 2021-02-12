580bec4d994a31d25961511e3f7b7c492c35caf6
BIGEARD Robin <robin.bigeard@ynov.com>
2021-02-12 16:05:45 +0100

    feat(): add seed database and run async

---
+++ Launcher.cs
@@ -0,0 +1,28 @@
+﻿using ProjetZORK.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Microsoft.Extensions.Hosting;
+
+namespace ProjetZORK
+{
+    public class Launcher
+    {
+        public Func<object, object, Task> Exit { get; internal set; }
+
+        public event EventHandler<Task> Event;
+
+        public void Start()
+        {
+            Console.Clear();
+            Console.WriteLine("Zork !");
+            Thread.Sleep(2000);
+            Console.Clear();
+            Console.WriteLine("Menu");
+            Console.WriteLine("______________________________________________");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,45 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+
+using ProjetZORK.Services.Extensions;
+using ProjetZORK.DataAccessLayer.Extensions;
+
+using System.Threading.Tasks;
 
 namespace ProjetZORK
 {
     class Program
     {
-        static void Main(string[] args)
+        static Task Main(string[] args)
         {
-            Game game = new Game();
-            _ = game;
+            using IHost host = CreateHostBuilder(args).Build().SeedDatabase();
+
+            var run = host.RunAsync();
+            
+            var launcher = host.Services.GetService<Launcher>();
+
+            launcher.Exit += (o, e) => host.StopAsync();
+
+            launcher.Start();
+
+            return run;
+            // new Menu();
+            // Game game = new Game();
+            // _ = game;
+        }
+        static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            return Host.CreateDefaultBuilder(args).ConfigureServices((_, services) => {
+                services.AddSingleton<Launcher>();
+                services.AddDataService();
+             });
         }
     }
 }
