4538e16a529774a5822c285d5dccd881ac10c7a8
BIGEARD Robin <robin.bigeard@ynov.com>
2021-02-12 13:28:19 +0100

    feat(): add monster and csproj

--- Monster.cs
+++ Monster.cs
@@ -1,3 +1,4 @@
+using ProjetZORK.Services.Dto;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ProjetZORK
 {
-    public class Monster
+    public class Monster : MonsterDto
     {
         public Monster()
         {
