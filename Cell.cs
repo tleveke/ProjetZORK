d9ed0a3674a3c5a7195855943a0b73033a9e707e
BIGEARD Robin <robin.bigeard@ynov.com>
2021-02-12 12:13:45 +0100

    feat(Game): init game

---
+++ Cell.cs
@@ -0,0 +1,20 @@
+﻿using ProjetZORK.Services.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetZORK
+{
+    public class Cell : CellDto
+    {
+        public Cell(int Id, int PosX, int PosY)
+        {
+            this.Id = Id;
+            this.PosX = PosX;
+            this.PosY = PosY;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ namespace ProjetZORK
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Game game = new Game();
+            _ = game;
         }
     }
 }
