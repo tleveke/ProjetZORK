1d102f9d1d12522f1909379feee7e35a626d60eb
tomleveque <tomleveque@TOMLEVEQUE8676>
2021-02-13 19:59:15 +0100

    feat(Game.cs) => create Player and Map

--- Game/Game.cs
+++ Game/Game.cs
@@ -18,42 +18,49 @@ namespace ProjetZORK
         {
             this.zorkService = zorkService;
             //Task.Run(() => generateMap(40, 40, 10));
-            generateMap(40, 40, 10);
+            generatePlayer();
+            //generateMap(40, 40, 10);
         }
 
-        public void generateMap(int width, int height, int numObstacle)
+        public void generatePlayer()
+        {
+            Console.Clear();
+            Console.WriteLine("##############################################");
+            Console.WriteLine("                  Choose Player Name        " );
+            Console.WriteLine("##############################################");
+            Console.Write("> ");
+            var namePlayer = Console.ReadLine();
+            Task.Run(async () => {
+                generateMap(5, 5, 0, await this.zorkService.PlayerServices.AddAsync(new PlayerDto { Name = namePlayer, XP = 1, HP = 1, MaxHP = 1 }) ); 
+            }).Wait();
+
+            //int playerID = await this.zorkService.PlayerServices.AddAsync(new PlayerDto { Name = namePlayer, XP = 1, HP = 1, MaxHP = 1 });
+
+            //generateMap(40, 40, 0, playerID);
+
+
+
+        }
+
+        public void generateMap(int width, int height, int numObstacle, int gameId)
         {
             //Task.Run(async () => { await this.zorkService.CellServices.AddAsync(new CellDto { PosX = 1, PosY = 1, gameId = 1, Description = "dssdsd", canMoveTo = true, MonsterRate = 0 }); }).Wait();
             //Task.Run(async () => { await this.zorkService.PlayerServices.AddAsync(new PlayerDto { Name = "nnn",XP=1,HP=1,MaxHP=1 }); }).Wait();
-            foreach (CellDto cell in this.zorkService.CellServices.GetAllGameId(1) )
+            
+            
+            /*foreach (CellDto cell in this.zorkService.CellServices.GetAllGameId(1) )
             {
                 Console.WriteLine(cell.Description);
-            }
-            /*
-             
-            Microsoft.EntityFrameworkCore.Database.Command[20102]
-      Failed executing DbCommand (152ms) [Parameters=[@p0='?' (Size = 4000), @p1='?' (DbType = Int32), @p2='?' (DbType = Int32), @p3='?' (DbType = Int32), @p4='?' (DbType = Boolean), @p5='?' (DbType = Int32)], CommandType='Text', CommandTimeout='30']
-      SET NOCOUNT ON;
-      INSERT INTO [Cells] ([Description], [MonsterRate], [PosX], [PosY], [canMoveTo], [gameId])
-      VALUES (@p0, @p1, @p2, @p3, @p4, @p5);
-      SELECT [Id]
-      FROM [Cells]
-      WHERE @@ROWCOUNT = 1 AND [Id] = scope_identity();
-fail: Microsoft.EntityFrameworkCore.Update[10000]
-      An exception occurred in the database while saving changes for context type 'ProjetZORK.DataAccessLayer.ZorkManagerDbContext'.
-      Microsoft.EntityFrameworkCore.DbUpdateException: An error occurred while updating the entries. See the inner exception for details.
-       ---> Microsoft.Data.SqlClient.SqlException (0x80131904): Nom d'objet 'Cells' non valide.
-             
-             */
+            }*/
 
-            /*for (int y = 0; y < height; y++)
+            for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
-                    Task.Run(async () => { await this.zorkService.CellServices.AddAsync(new CellDto { PosX = x, PosY = y, gameId = 1, Description = "dssdsd", canMoveTo = true, MonsterRate = 0 }); }).Wait();
-                    //await this.zorkService.CellServices.AddAsync(new CellDto { PosX = x, PosY = y, gameId = 1, Description = "dssdsd", canMoveTo = true, MonsterRate = 0 });
+                    Task.Run(async () => { await this.zorkService.CellServices.AddAsync(new CellDto { PosX = x, PosY = y, gameId = gameId, Description = "dssdsd", canMoveTo = true, MonsterRate = 0 }); }).Wait();
                 }
-            }*/
+            }
+            Console.WriteLine("Fin de Génération");
         }
     }
 }
